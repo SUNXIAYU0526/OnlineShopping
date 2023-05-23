6b2b98e21a5b122fece7f613a2327472cf6fa75b
unknown <sunxiayu0526@gmail.com>
2023-05-23 11:54:06 +0900

    commit v2

---
+++ src/main/java/com/example/demo/KkkkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkkkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkkkkApplication.class, args);
+	}
+
+}
